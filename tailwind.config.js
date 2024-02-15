72d1a0ee2b8938ed87b9e6afc8fab64f8ef91756
Krishna <krishna@findcoder.io>
2024-02-15 18:09:08 +0530

    new page created

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        sm: "0 -5px 20px 10px rgb(18 38 94/0.7)",
+      },
+    },
   },
   plugins: [],
 };
